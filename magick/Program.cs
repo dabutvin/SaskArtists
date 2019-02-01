d3e3ea5aaf9f84e8b0ba8a7171e56fe0bf282420
dabutvin <butvinik@outlook.com>
2019-01-31 18:16:08 -0800

    debug program

---
+++ magick/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageMagick;
+
+namespace magick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imageOptimizer = new ImageOptimizer
+            {
+                OptimalCompression = true,
+                IgnoreUnsupportedFormats = true,
+            };
+
+            var files = Directory.EnumerateFiles(Path.GetFullPath("../www/"), "*.png", SearchOption.AllDirectories);
+            Console.WriteLine($"start: {files.Count()}");
+            var count = 0;
+            Parallel.ForEach(files, file =>
+            {
+                count++;
+                Console.WriteLine($"${count}: {file}");
+                try
+                {
+                    imageOptimizer.LosslessCompress(new FileInfo(file));
+                }
+                catch
+                {
+                    // ignore
+                }
+            });
+            Console.WriteLine("finished!!");
+        }
+    }
+}
